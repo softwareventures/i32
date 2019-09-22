e02be9daebf304bf637a5a37726101d7c57bae85
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-09-22 13:28:59 +0100

    feat(isum): add isum function

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import {fold} from "@softwareventures/array";
+
 export function inot(value: number): number {
     return ((!(value | 0)) as any) | 0;
 }
@@ -43,6 +45,10 @@ export function imod(a: number, b: number): number {
 
 export function iadd(a: number, b: number): number {
     return ((a | 0) + (b | 0)) | 0;
+}
+
+export function isum(...values: number[]): number {
+    return fold(values, iadd, 0);
 }
 
 export function isub(a: number, b: number): number {
