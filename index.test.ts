1e85a1e6dbe9f20ee0afde180d444b235fcdf299
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-09-22 10:30:44 +0100

    feat(iadd): add iadd function

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import {inot} from "./index";
+import {iadd, inot} from "./index";
 
 test("inot", t => {
     t.is(inot(0), 1);
@@ -7,4 +7,13 @@ test("inot", t => {
     t.is(inot(-1), 0);
     t.is(inot(342), 0);
     t.is(inot(-342), 0);
+});
+
+test("iadd", t => {
+    t.is(iadd(0, 0), 0);
+    t.is(iadd(0, 21), 21);
+    t.is(iadd(0, -21), -21);
+    t.is(iadd(1, 2), 3);
+    t.is(iadd(1e10, 1), 1410065409);
+    t.is(iadd(123, 456), 579);
 });
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 export function inot(value: number): number {
     return ((!(value | 0)) as any) | 0;
 }
+
+export function iadd(a: number, b: number): number {
+    return ((a | 0) + (b | 0)) | 0;
+}
