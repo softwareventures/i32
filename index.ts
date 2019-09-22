da7cf68ca16669ad4edd798f03f698f395070ee6
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-09-22 11:36:28 +0100

    feat(imul): add imul function

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import _imul = require("imul");
+
 export function inot(value: number): number {
     return ((!(value | 0)) as any) | 0;
 }
@@ -9,6 +11,8 @@ export function icmp(value: number): number {
 export function ineg(value: number): number {
     return (-(value | 0)) | 0;
 }
+
+export const imul = _imul;
 
 export function iadd(a: number, b: number): number {
     return ((a | 0) + (b | 0)) | 0;
